1af1dc682625d71b33f9b6b6a7f10c30c6dc150d
van <463912679@qq.com>
2019-07-11 19:17:59 +0800

    微调后可能找不到重合区的点，那么固定设置上下限

--- overLapRegion.cpp
+++ overLapRegion.cpp
@@ -329,9 +329,20 @@ float max_panel_length=render.GetPanoLen();
 		cur_x_min=cur_x_min < 0? 0 : cur_x_min;
 		cur_y_min = cur_y_min<0?0:cur_y_min;
 
-		cur_x_max=cur_x_max>SDI_WIDTH? SDI_WIDTH:cur_x_max;
-		cur_y_max=cur_y_max>SDI_HEIGHT? SDI_HEIGHT:cur_y_max;
+		cur_x_min=cur_x_min>SDI_WIDTH? SDI_WIDTH-10:cur_x_min;
+		cur_y_min=cur_y_min>SDI_HEIGHT? SDI_HEIGHT-10:cur_y_min;
+
+		cur_x_max=cur_x_max>SDI_WIDTH? SDI_WIDTH-10:cur_x_max;
+		cur_y_max=cur_y_max>SDI_HEIGHT? SDI_HEIGHT-10:cur_y_max;
+
+		cur_x_max=cur_x_max < 0? 10 : cur_x_max;
+		cur_y_max = cur_y_max<0? 10:cur_y_max;
+
+		cur_x_max=cur_x_max>cur_x_min? cur_x_max:cur_x_min+8;
+		cur_y_max=cur_y_max>cur_y_min? cur_y_max:cur_y_min+8;
 	sprintf(buf_1,"./data/save/%d_left.bmp",direction);
+
+
 	roi_image[direction][LEFT_ROI]=van_images[direction](
 			Range(cur_y_min,cur_y_max),
 			Range(cur_x_min,cur_x_max)////leftmid
